644e33c323c2d7446f3b7e05c0bb390e0a39c89f
Isaac Karth <isaac@isaackarth.com>
2023-07-01 22:24:07 -0700

    fix incorrect import

--- guidance/llms/__init__.py
+++ guidance/llms/__init__.py
@@ -3,7 +3,6 @@ from ._text_generation_web_ui import TextGenerationWebUI
 from ._transformers import Transformers
 from ._mock import Mock
 from ._llm import LLM, LLMSession, SyncSession
-from ._azure_openai import AzureOpenAI
 from ._deep_speed import DeepSpeed
 from . import transformers
 from . import caches
